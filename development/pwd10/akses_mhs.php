e45999ba11c143fa47088c002fbcc791a92d8172
lugasalhawariy <lugasalhawariy@gmail.com>
2021-12-23 22:04:38 +0700

    pwd10: perubahan

--- development/pwd10/akses_mhs.php
+++ development/pwd10/akses_mhs.php
@@ -1,11 +1,12 @@
 <?php
 
-$url = "http://localhost:5000/pwd10/getdatamhs.php";
+$url = "http://localhost/pwd10/getdatamhs.php";
 $client = curl_init($url);
 curl_setopt($client, CURLOPT_RETURNTRANSFER, 1);
 
 $response = curl_exec($client);
-$result = json_decode($response); foreach ($result as $r) {
+$result = json_decode($response); 
+foreach ($result as $r) {
 	echo "<p>";
 	echo "NIM : " . $r->nim . "<br />";
 	echo "Nama : " . $r->nama . "<br />"; 
